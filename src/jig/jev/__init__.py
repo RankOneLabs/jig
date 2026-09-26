c75cff9651eaccfd148a0d611fd78a7e6f0b5cb8
Steven Ciraolo <steve@rankonelabs.com>
2026-09-25 20:46:10 -0700

    feat(jev): lock public submodule exports

--- src/jig/jev/__init__.py
+++ src/jig/jev/__init__.py
@@ -1,13 +1,10 @@
 """TypeSafe Jev client, protocol types, and wire transforms."""
 
-from jig.jev.errors import JevError, JevErrorKind
 from jig.jev.client import DEFAULT_ENDPOINT, JevClient
+from jig.jev.errors import JevError, JevErrorKind
 from jig.jev.models import (
     ChoiceAnswer,
     ChoiceQuestion,
-    JevAnswer,
-    JevJson,
-    JevQuestion,
     JevResult,
     JevUsage,
     NoulAnswer,
@@ -16,11 +13,9 @@ from jig.jev.models import (
     ScoreQuestion,
 )
 from jig.jev.tracing import to_jig_usage
-from jig.jev.wire import PROBABILITY_SUM_TOLERANCE, build_request_body, parse_response
-
+from jig.jev.wire import build_request_body, parse_response
 __all__ = [
-    "ChoiceAnswer", "ChoiceQuestion", "DEFAULT_ENDPOINT", "JevAnswer", "JevClient", "JevError", "JevErrorKind",
-    "JevJson", "JevQuestion", "JevResult", "JevUsage", "NoulAnswer",
-    "NoulQuestion", "PROBABILITY_SUM_TOLERANCE", "ScoreAnswer",
-    "ScoreQuestion", "build_request_body", "parse_response", "to_jig_usage",
+    "JevClient", "NoulQuestion", "ChoiceQuestion", "ScoreQuestion",
+    "NoulAnswer", "ChoiceAnswer", "ScoreAnswer", "JevResult", "JevUsage",
+    "JevError", "JevErrorKind", "to_jig_usage",
 ]
